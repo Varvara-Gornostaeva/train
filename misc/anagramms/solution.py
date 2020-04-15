5038aead784a3c6350fb3a7eef9555a3824250ec
VarvaraMalysheva <varvara.malysheva@saritasa.com>
2020-04-15 15:13:02 +0700

    Add anagramms
    Update duplicate removal

---
+++ misc/anagramms/solution.py
@@ -0,0 +1,33 @@
+"""https://contest.yandex.ru/contest/8458/problems/E/"""
+from collections import Counter
+
+
+def is_anagram_counter(a, b):
+    if len(a) != len(b):
+        print(0)
+        return
+    ca = Counter(a)
+    cb = Counter(b)
+    diff = ca-cb
+    print(1 if len(diff) == 0 else 0)
+
+
+def is_anagram_while(first, second):
+    l1, l2 = list(first), list(second)
+    while l1:
+        li = l1.pop()
+        if li in l2:
+            l2.remove(li)
+        else:
+            print(0)
+            return
+    print(1 if not l2 else 0)
+
+
+if __name__ == "__main__":
+    file = open("input.txt", "r")
+    first = str(file.readline().strip())
+    second = str(file.readline().strip())
+    is_anagram_counter(first, second)
+    is_anagram_while(first, second)
+
--- misc/duplicate_removal/solution.py
+++ misc/duplicate_removal/solution.py
@@ -1,28 +1,19 @@
 """Source https://contest.yandex.ru/contest/8458/problems/C/"""
 
-file = open("input.txt", "r")
-len_mass = int(file.readline().strip())
-previous = -1
-clean = []
-for i in range(len_mass):
-    item = int(file.readline().strip())
-    if item != previous:
-        clean.append(item)
-    previous = item
-for i in clean:
-    print(i)
+
+def duplicate_removal():
+    file = open("input.txt", "r")
+    len_mass = int(file.readline().strip())
+    previous = -1
+    clean = []
+    for i in range(len_mass):
+        item = int(file.readline().strip())
+        if item != previous:
+            clean.append(item)
+        previous = item
+    for i in clean:
+        print(i)
 
 
-# solution for context
-import sys
-len_mass = int(sys.stdin.readline().strip())
-previous = -1
-clean = []
-for i in range(len_mass):
-    item = sys.stdin.readline().strip()
-    if item != previous:
-        clean.append(item)
-    previous = item
-
-for i in clean:
-    print(i)
+if __name__ == "__main__":
+    duplicate_removal()
