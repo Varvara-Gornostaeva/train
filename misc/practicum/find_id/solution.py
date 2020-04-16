17f8f7849743f1313b4c2225e27d627cdb9d7519
VarvaraMalysheva <varvara.malysheva@saritasa.com>
2020-04-16 11:42:39 +0700

    Add find id god

--- misc/practicum/find_id/solution.py
+++ misc/practicum/find_id/solution.py
@@ -10,5 +10,19 @@ def find_id_slow():
             print(i, end=' ')
 
 
+# O(2*n)
+def find_id():
+    file = open("input.txt", "r")
+    n = int(file.readline().strip())
+    arr_full = [0]*(n+1)
+    arr = list(map(int, file.readline().split()))
+    for i in arr:
+        arr_full[i] += 1
+    del arr
+    for i in range(1, n+1):
+        if not arr_full[i]:
+            print(i, end=' ')
+
+
 if __name__ == "__main__":
-    find_id_slow()
+    find_id()
